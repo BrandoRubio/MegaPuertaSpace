1fd5930b86ea927335a6b82825060220e1f0744d
BrandoRubio <sanchezbrando197@gmail.com>
2022-04-13 13:44:50 -0500

    android changes
    cambios para android

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -8,5 +8,36 @@ import { Component } from '@angular/core';
 export class Tab1Page {
 
   constructor() {}
-
+  
+  estado1 = "Abierta"
+  colorP1 = ""
+  estado2 = "Abierta"
+  colorP2 = ""
+  estado3 = "Abierta"
+  colorP3 = ""
+  estado4 = "Abierta"
+  colorP4 = ""
+  
+  ngOnInit(){
+    if(this.estado1=="Abierta"){
+      this.colorP1 = "success"
+    }else{
+      this.colorP1 = "danger"
+    }
+    if(this.estado2=="Abierta"){
+      this.colorP2 = "success"
+    }else{
+      this.colorP3 = "danger"
+    }
+    if(this.estado3=="Abierta"){
+      this.colorP3 = "success"
+    }else{
+      this.colorP4 = "danger"
+    }
+    if(this.estado4=="Abierta"){
+      this.colorP4 = "success"
+    }else{
+      this.colorP1 = "danger"
+    }
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,44 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public storage: Storage) {}
+  ipPuerta1
+  ipPuerta2
+  ipPuerta3
+  ipPuerta4
+
+  saveIP1(ip){
+    console.log(ip);
+  }
+  saveIP2(ip){
+    console.log(ip);
+  }
+  saveIP3(ip){
+    console.log(ip);
+  }
+  saveIP4(ip){
+    console.log(ip);
+  }
+  ionViewDidEnter() {
+    this.storage.get('ippuerta1').then(res => {
+      this.ipPuerta1 = res;
+    })
+    this.storage.get('ippuerta2').then(res => {
+      this.ipPuerta2 = res;
+    })
+    this.storage.get('ippuerta3').then(res => {
+      this.ipPuerta3 = res;
+    })
+    this.storage.get('ippuerta4').then(res => {
+      this.ipPuerta4 = res;
+    })
+  }
+
+  ionViewDidLeave() {
+    this.storage.set('ippuerta1', this.ipPuerta1)
+    this.storage.set('ippuerta2', this.ipPuerta2)
+    this.storage.set('ippuerta3', this.ipPuerta3)
+    this.storage.set('ippuerta4', this.ipPuerta4)
+  }
 
 }
